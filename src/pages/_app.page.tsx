806e59f83f24ff4cb12a9d9e859fc1389a0469eb
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2024-01-08 20:47:12 -0300

    fix: remove messages import of app_page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -5,7 +5,6 @@ import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { NextIntlClientProvider } from "next-intl";
 import { useRouter } from "next/router";
-import { GetStaticPropsContext } from "next";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
