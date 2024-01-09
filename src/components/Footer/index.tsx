2f441bb9e017d224f978ee1dc4329022729f62bb
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2024-01-09 16:08:32 -0300

    style: adjusts in header

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import { useDarkMode } from "@/src/context/DarkModeContext";
 import Link from "next/link";
 import {
   AiFillGithub,
@@ -7,15 +8,20 @@ import {
 import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
+  const { isDarkMode } = useDarkMode();
   return (
     <footer className="mb-4">
       <p>Você pode entrar em contato com Gabriel nos seguintes canais: </p>
 
-      <nav className="flex items-center mt-4">
+      <nav
+        className={`flex items-center mt-4  ${
+          isDarkMode ? "text-white" : null
+        }`}
+      >
         <Link
           href="https://www.linkedin.com/in/gabriel-dison/"
           target="_blank"
-          className="px-2 flex items-center gap-2 underline"
+          className="px-2 flex items-center gap-2 hover:underline"
         >
           LinkedIn
           <AiFillLinkedin />
@@ -23,7 +29,7 @@ const Footer = () => {
         <Link
           href="https://github.com/Gabrieldison"
           target="_blank"
-          className="px-2 flex items-center gap-2 underline"
+          className="px-2 flex items-center gap-2 hover:underline"
         >
           Github
           <AiFillGithub />
@@ -31,7 +37,7 @@ const Footer = () => {
         <Link
           href="mailto:gabriel.disonreis@gmail.com"
           target="_blank"
-          className="px-2 flex items-center gap-2 underline"
+          className="px-2 flex items-center gap-2 hover:underline"
         >
           Email
           <MdEmail />
@@ -39,7 +45,7 @@ const Footer = () => {
         <Link
           href="https://wa.me/5584981549159"
           target="_blank"
-          className="px-2 flex items-center gap-2 underline"
+          className="px-2 flex items-center gap-2 hover:underline"
         >
           WhatsApp
           <AiOutlineWhatsApp />
